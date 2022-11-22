5b77bf15cca78172c9904eedff8d71024164145e
Mike Lysikov <d8corp@mail.ru>
2022-11-22 18:33:18 +0300

    improve menu

--- src/app/Menu/Menu.tsx
+++ src/app/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import { Space } from 'src/ui'
+import { Flex, Space } from 'src/ui'
 
 import styles from './Menu.scss'
 
@@ -10,30 +10,30 @@ const linkClass = {
 export function Menu () {
   return (
     <nav class={styles.root}>
-      <a href='/'>Home</a>
-      <section>
-        <a class={linkClass} href='/typography'>Typography</a>
+      <a class={linkClass} exact href='/'>Home</a>
+      <a class={linkClass} href='/typography'>Typography</a>
+      <section class={styles.submenu}>
         <a class={linkClass} href='/typography/markdown'>Markdown</a>
         <a class={linkClass} href='/typography/typography'>Typography</a>
       </section>
-      <section>
-        <a class={linkClass} href='/position'>Position</a>
+      <a class={linkClass} href='/position'>Position</a>
+      <section class={styles.submenu}>
         <a class={linkClass} href='/position/flex'>Flex</a>
         <a class={linkClass} href='/position/space'>Space</a>
       </section>
-      <section>
-        <a class={linkClass} href='/buttons'>Buttons</a>
+      <a class={linkClass} href='/buttons'>Buttons</a>
+      <section class={styles.submenu}>
         <a class={linkClass} href='/buttons/button'>Button</a>
       </section>
-      <section>
-        <a class={linkClass} href='/content'>Content</a>
-        <a class={linkClass} href='/content/spin'>Spin</a>
+      <a class={linkClass} href='/content'>Content</a>
+      <section class={styles.submenu}>
+        <a class={linkClass} exact href='/content/spin'>Spin</a>
         <a class={linkClass} href='/content/spin-box'>SpinBox</a>
       </section>
       <Space />
-      <span>
-        {process.env.CANTINC_UI_VERSION}
-      </span>
+      <Flex justify='center'>
+        v{process.env.CANTINC_UI_VERSION}
+      </Flex>
     </nav>
   )
 }
