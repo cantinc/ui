5c5662fa283946b3066cce7fc8472a8cf5dc6cb1
Mike Lysikov <d8corp@mail.ru>
2022-11-23 19:43:36 +0300

    improve menu

--- src/app/LayoutMenu/LayoutMenu.tsx
+++ src/app/LayoutMenu/LayoutMenu.tsx
@@ -6,29 +6,29 @@ export function LayoutMenu () {
       <Menu.Item exact href='/'>Home</Menu.Item>
       <Menu.Item href='/typography'>Typography</Menu.Item>
       <Menu.SubMenu>
-        <Menu.Item href='/typography/markdown'>Markdown</Menu.Item>
-        <Menu.Item href='/typography/typography'>Typography</Menu.Item>
+        <Menu.Item href='/typography/markdown'>{'<'}Markdown {'/>'}</Menu.Item>
+        <Menu.Item href='/typography/typography'>{'<'}Typography {'/>'}</Menu.Item>
       </Menu.SubMenu>
       <Menu.Item href='/position'>Position</Menu.Item>
       <Menu.SubMenu>
-        <Menu.Item href='/position/flex'>Flex</Menu.Item>
-        <Menu.Item href='/position/space'>Space</Menu.Item>
+        <Menu.Item href='/position/flex'>{'<'}Flex {'/>'}</Menu.Item>
+        <Menu.Item href='/position/space'>{'<'}Space {'/>'}</Menu.Item>
       </Menu.SubMenu>
       <Menu.Item href='/popups'>Popups</Menu.Item>
       <Menu.SubMenu>
-        <Menu.Item href='/popups/notifications'>Notifications</Menu.Item>
+        <Menu.Item href='/popups/notifications'>{'<'}Notifications {'/>'}</Menu.Item>
       </Menu.SubMenu>
       <Menu.Item href='/buttons'>Buttons</Menu.Item>
       <Menu.SubMenu>
-        <Menu.Item href='/buttons/button'>Button</Menu.Item>
+        <Menu.Item href='/buttons/button'>{'<'}Button {'/>'}</Menu.Item>
       </Menu.SubMenu>
       <Menu.Item href='/content'>Content</Menu.Item>
       <Menu.SubMenu>
-        <Menu.Item href='/content/divider'>Divider</Menu.Item>
-        <Menu.Item exact href='/content/spin'>Spin</Menu.Item>
-        <Menu.Item href='/content/spin-box'>SpinBox</Menu.Item>
-        <Menu.Item href='/content/toggle-bar'>ToggleBar</Menu.Item>
-        <Menu.Item href='/content/search-toggle-bar'>SearchToggleBar</Menu.Item>
+        <Menu.Item href='/content/divider'>{'<'}Divider {'/>'}</Menu.Item>
+        <Menu.Item exact href='/content/spin'>{'<'}Spin {'/>'}</Menu.Item>
+        <Menu.Item href='/content/spin-box'>{'<'}SpinBox {'/>'}</Menu.Item>
+        <Menu.Item href='/content/toggle-bar'>{'<'}ToggleBar {'/>'}</Menu.Item>
+        <Menu.Item href='/content/search-toggle-bar'>{'<'}SearchToggleBar {'/>'}</Menu.Item>
       </Menu.SubMenu>
       <Space />
       <Flex justify='center'>
