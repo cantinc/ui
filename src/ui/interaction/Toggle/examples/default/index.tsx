67480846f6f2250f46574fdd3f33bd570a5ec3f7
Mike Lysikov <d8corp@mail.ru>
2022-12-10 18:27:14 +0300

    refactor examples of toggle

--- src/ui/interaction/Toggle/examples/default/index.tsx
+++ src/ui/interaction/Toggle/examples/default/index.tsx
@@ -10,6 +10,21 @@ export default example({
   id: 'default',
   title: 'Default',
   description,
+  code: `import innet from 'innet'
+import dom from '@innet/dom'
+import { State } from 'watch-state'
+
+import { Toggle } from '@cantinc/ui'
+
+const state = new State(false)
+
+innet(
+  <Toggle
+    value={() => state.value}
+    onchange={value => { state.value = value }}
+  />,
+  dom,
+)`,
   example: (
     <Toggle
       value={() => state.value}
--- src/ui/interaction/ToggleBar/examples/default/index.tsx
+++ src/ui/interaction/ToggleBar/examples/default/index.tsx
@@ -10,6 +10,30 @@ export default example({
   id: 'default',
   title: 'ToggleBar',
   description,
+  code: `import innet from 'innet'
+import dom from '@innet/dom'
+import { State } from 'watch-state'
+
+import { ToggleBar } from '@cantinc/ui'
+
+const state = new State('')
+
+innet(
+  <ToggleBar
+    element='nav'
+    gap={16}
+    value={() => state.value}
+    onchange={value => {
+      state.value = value
+    }}
+    values={[
+      { value: 'on' },
+      { value: '', label: 'auto' },
+      { value: 'off' },
+    ]}
+  />,
+  dom,
+)`,
   example: (
     <ToggleBar
       element='nav'
