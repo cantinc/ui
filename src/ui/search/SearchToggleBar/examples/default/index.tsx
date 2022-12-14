f1b123215c1d4757fe4711a4b3d39d3a9c3e2c25
Mike Lysikov <d8corp@mail.ru>
2022-12-14 17:10:18 +0300

    improve SearchToggleBar docs

--- src/ui/search/SearchToggleBar/examples/default/index.tsx
+++ src/ui/search/SearchToggleBar/examples/default/index.tsx
@@ -7,6 +7,22 @@ export default example({
   id: 'default',
   title: 'Default',
   description,
+  code: `import innet from 'innet'
+import dom from '@innet/dom'
+
+import { SearchToggleBar } from '@cantinc/ui'
+
+innet(
+  <SearchToggleBar
+    key='test'
+    values={[
+      { value: 'on' },
+      { value: '', label: 'auto' },
+      { value: 'off' },
+    ]}
+  />,
+  dom,
+)`,
   example: (
     <SearchToggleBar
       key='test'
