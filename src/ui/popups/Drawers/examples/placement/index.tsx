b35f91a1dea1558a6628365d6229bd60fe636631
Mike Lysikov <d8corp@mail.ru>
2022-12-22 13:15:58 +0300

    fixes for mobile

--- src/ui/popups/Drawers/examples/placement/index.tsx
+++ src/ui/popups/Drawers/examples/placement/index.tsx
@@ -25,60 +25,60 @@ const right = new State(false)
 const bottom = new State(false)
 
 innet(
-  <>
-    <Flex gap={16}>
-      <Button onclick={() => { left.value = true }}>
-        Left
-      </Button>
-      <Button onclick={() => { top.value = true }}>
-        Top
-      </Button>
-      <Button onclick={() => { right.value = true }}>
-        Right
-      </Button>
-      <Button onclick={() => { bottom.value = true }}>
-        Bottom
-      </Button>
-    </Flex>
-    <Drawers>
-      <show state={left}>
-        <Drawer
-          padding={24}
-          placement='left'
-          onclose={() => { left.value = false }}>
-          Left
-        </Drawer>
-      </show>
-      <show state={top}>
-        <Drawer
-          padding={24}
-          placement='top'
-          onclose={() => { top.value = false }}>
-          Top
-        </Drawer>
-      </show>
-      <show state={right}>
-        <Drawer
-          padding={24}
-          placement='right'
-          onclose={() => { right.value = false }}>
-          Right
-        </Drawer>
-      </show>
-      <show state={bottom}>
-        <Drawer
-          padding={24}
-          placement='bottom'
-          onclose={() => { bottom.value = false }}>
-          Bottom
-        </Drawer>
-      </show>
-    </Drawers>
-  </>,
+  <Flex wrap gap={16}>
+    <portal parent={document.body}>
+      <Drawers>
+        <show state={left}>
+          <Drawer
+            padding={24}
+            placement='left'
+            onclose={() => { left.value = false }}>
+            Left
+          </Drawer>
+        </show>
+        <show state={top}>
+          <Drawer
+            padding={24}
+            placement='top'
+            onclose={() => { top.value = false }}>
+            Top
+          </Drawer>
+        </show>
+        <show state={right}>
+          <Drawer
+            padding={24}
+            placement='right'
+            onclose={() => { right.value = false }}>
+            Right
+          </Drawer>
+        </show>
+        <show state={bottom}>
+          <Drawer
+            padding={24}
+            placement='bottom'
+            onclose={() => { bottom.value = false }}>
+            Bottom
+          </Drawer>
+        </show>
+      </Drawers>
+    </portal>
+    <Button onclick={() => { left.value = true }}>
+      Left
+    </Button>
+    <Button onclick={() => { top.value = true }}>
+      Top
+    </Button>
+    <Button onclick={() => { right.value = true }}>
+      Right
+    </Button>
+    <Button onclick={() => { bottom.value = true }}>
+      Bottom
+    </Button>
+  </Flex>,
   dom,
 )`,
   example: (
-    <Flex gap={16}>
+    <Flex wrap gap={16}>
       <portal parent={document.body}>
         <Drawers>
           <show state={left}>
