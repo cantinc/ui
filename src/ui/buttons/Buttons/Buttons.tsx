1a9e9b6d46b4f6d86f231fdbcf0dc22e4f691fa0
Mike Lysikov <d8corp@mail.ru>
2023-01-14 13:24:33 +0300

    improve Buttons

--- src/ui/buttons/Buttons/Buttons.tsx
+++ src/ui/buttons/Buttons/Buttons.tsx
@@ -1,6 +1,10 @@
+import { style } from '@innet/dom'
 import { useChildren } from '@innet/jsx'
 
 import { Flex, FlexProps } from '../../layout'
+import styles from './Buttons.scss'
+
+const useStyles = style(styles)
 
 export interface ButtonsProps extends FlexProps {
 
@@ -8,9 +12,10 @@ export interface ButtonsProps extends FlexProps {
 
 export function Buttons (props: ButtonsProps) {
   const children = useChildren()
+  const styles = useStyles()
 
   return (
-    <Flex wrap gap={24} {...props}>
+    <Flex wrap gap={24} {...props} class={() => styles.root}>
       {children}
     </Flex>
   )
