1311774b8e8ac215d81d379735ffc94a647c055d
Mike Lysikov <d8corp@mail.ru>
2023-03-12 16:00:49 +0300

    improve Stories example

--- src/ui/interaction/Stories/examples/default/index.tsx
+++ src/ui/interaction/Stories/examples/default/index.tsx
@@ -1,5 +1,6 @@
 import { example } from 'src/app/Component'
 import { Stories, type Story } from 'src/ui'
+import { State } from 'watch-state'
 
 import description from './README.md'
 import story1 from './story1.webp'
@@ -12,25 +13,30 @@ import story2_2 from './story2-2.webp'
 import story3 from './story3.webp'
 import story3_1 from './story3_1.webp'
 
+const readStories = new State(new Set<number>())
+const read = (id: number) => {
+  readStories.value.add(id)
+  readStories.update()
+}
+
 const stories: Story[] = [
   {
     preview: story1,
     slides: [story1_1, story1_2, story1_3],
-    onread: () => console.log(1),
   },
   {
     preview: story2,
-    unread: true,
     slides: [story2_1, story2_2],
-    onread: () => console.log(2),
   },
   {
     preview: story3,
-    unread: true,
     slides: [story3_1],
-    onread: () => console.log(3),
   },
-]
+].map((props, index) => ({
+  ...props,
+  unread: () => !readStories.value.has(index),
+  onread: () => read(index),
+}))
 
 export default example({
   id: 'default',
@@ -38,6 +44,7 @@ export default example({
   description,
   code: `import innet from 'innet'
 import dom from '@innet/dom'
+import { State } from 'watch-state'
 
 import { Stories, Story } from '@cantinc/ui'
 
@@ -51,6 +58,12 @@ import story2_2 from './story2-2.jpg'
 import story3 from './story3.webp'
 import story3_1 from './story3_1.jpg'
 
+const readStories = new State(new Set<number>())
+const read = (id: number) => {
+  readStories.value.add(id)
+  readStories.update()
+}
+
 const stories: Story[] = [
   {
     preview: story1,
@@ -58,15 +71,17 @@ const stories: Story[] = [
   },
   {
     preview: story2,
-    unread: true,
     slides: [story2_1, story2_2],
   },
   {
     preview: story3,
-    unread: true,
     slides: [story3_1],
   },
-]
+].map((props, index) => ({
+  ...props,
+  unread: () => !readStories.value.has(index),
+  onread: () => read(index),
+}))
 
 innet(
   <Stories stories={stories} />,
