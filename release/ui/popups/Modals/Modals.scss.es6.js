5c5615c9a081cda51764e15a692501e7365a3d9d
Deight <d8corp@mail.ru>
2025-07-15 23:39:13 +0300

    v0.4.1

--- release/ui/popups/Modals/Modals.scss.es6.js
+++ release/ui/popups/Modals/Modals.scss.es6.js
@@ -1,7 +1,7 @@
 import injector_8363bc92 from 'rollup-plugin-styles/dist/runtime/inject-css.js';
 
-var css = ".Modals_root__b8ae2da1{background:rgba(74,47,124,.9)}@media (max-width:767px){.Modals_root__b8ae2da1{background:var(--color-100)}}";
-var modules_383942b3 = {"root":"Modals_root__b8ae2da1"};
+var css = ".Modals_root__d630fcd7{background:rgba(0,0,0,.5)}@media (max-width:767px){.Modals_root__d630fcd7{background:var(--color-100)}}";
+var modules_383942b3 = {"root":"Modals_root__d630fcd7"};
 injector_8363bc92(css,{});
 
 export { css, modules_383942b3 as default };
--- release/ui/popups/Modals/Modals.scss.js
+++ release/ui/popups/Modals/Modals.scss.js
@@ -8,8 +8,8 @@ function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'defau
 
 var injector_8363bc92__default = /*#__PURE__*/_interopDefaultLegacy(injector_8363bc92);
 
-var css = ".Modals_root__b8ae2da1{background:rgba(74,47,124,.9)}@media (max-width:767px){.Modals_root__b8ae2da1{background:var(--color-100)}}";
-var modules_383942b3 = {"root":"Modals_root__b8ae2da1"};
+var css = ".Modals_root__d630fcd7{background:rgba(0,0,0,.5)}@media (max-width:767px){.Modals_root__d630fcd7{background:var(--color-100)}}";
+var modules_383942b3 = {"root":"Modals_root__d630fcd7"};
 injector_8363bc92__default["default"](css,{});
 
 exports.css = css;
